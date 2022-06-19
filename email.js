e7585afa34852ca90ff8152aa998f490e225acd3
Chirantan Banik <banikchirantan16@gmail.com>
2022-06-19 23:00:33 +0530

    Update email.js

--- email.js
+++ email.js
@@ -1,23 +1,22 @@
 var express = require('express');  // importing express and saving it.
-var mysql = require('mysql');
-var bodyParser = require("body-parser");
-var email = express();  // executing the entire express package taht we have just imported.
+var mysql = require('mysql');      // importing mysql.
+var bodyParser = require("body-parser");   // importing bodyparser
+var email = express();  // executing the entire express package that we have just imported.
 
 email.set("view engine", "ejs");   // set some settings in our app
 email.use(bodyParser.urlencoded({extended: true}));
-email.use(express.static(__dirname + "/public"));
+email.use(express.static(__dirname + "/public"));    // we kept the static files in public folder so taking it from there for using.
 
-var connection = mysql.createConnection({   // For Node.js and MySQL connection
+var connection = mysql.createConnection({     // line 10-14 for Node.js and MySQL connection
   host     : 'localhost',
   user     : 'root',
   database : 'email_database'
 });
 
 
-
-email.get("/", function(request, response){    // route 
-	// Find count of users in the database
-	var q = "Select count(*) As count From users";
+email.get("/", function(request, response){   // route 
+	
+	var q = "Select count(*) As count From users";   // Find count of users in the database
 	connection.query(q, function(err, results){
 		if(err) throw err;
 		// Respond with that count
@@ -25,7 +24,7 @@ email.get("/", function(request, response){    // route
 		//response.send("We have" + count + "users in the database"); 
 		response.render("home", {count: count});
 	});
-		});
+});
 
 email.post("/register", function(request, response){
 
@@ -34,11 +33,11 @@ var person = {
 	email: request.body.email        // For Inserting new email 
 };    
 	
-connection.query('Insert into users Set ?', person, function(err, result) {
+connection.query('Insert into users Set ?', person, function(err, result) {          // for inserting new email
   if(err) throw err;
   response.redirect("/");
+    });
 });
-	});
 	
 	//console.log("Post Request sent to /Register email is " + request.body.email);
 
